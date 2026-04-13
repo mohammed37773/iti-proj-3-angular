182e16957ee290600130c866352aa8964051c791
Nour Ahmed Mohamed Mahoumed <na3547697@gmail.com>
2026-04-13 15:12:12 +0200

    Nour-Abdo-ELtrurky

--- src/app/components/pages/patient/patient-pages.routes.ts
+++ src/app/components/pages/patient/patient-pages.routes.ts
@@ -4,34 +4,31 @@ export const routes: Routes = [
   {
     path: "",
     children: [
-      { path: "", redirectTo: "appointments", pathMatch: "full" },
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
 
       {
         path: "appointments",
-        loadComponent: () =>
-          import("./patient-appointments/patient-appointments")
-            .then(c => c.PatientAppointments)
+        loadComponent: () => import("./patient-appointments/patient-appointments").then(c => c.PatientAppointments)
       },
-
+      {
+        path: "dashboard",
+        loadComponent: () => import("./patient-dashboard/patient-dashboard").then(c => c.PatientDashboard)
+      },
       {
         path: "records",
-        loadComponent: () =>
-          import("./patient-records/patient-records")
-            .then(c => c.PatientRecords)
+        loadComponent: () => import("./patient-records/patient-records").then(c => c.PatientRecords)
       },
 
+      // ✅ user نفسه
       {
         path: "profile",
-        loadComponent: () =>
-          import("./patient-profile/patient-profile")
-            .then(c => c.PatientProfile)
+        loadComponent: () => import("./patient-profile/patient-profile").then(c => c.PatientProfile)
       },
 
+      // 🔥 optional (admin view)
       {
         path: "profile/:id",
-        loadComponent: () =>
-          import("./patient-profile/patient-profile")
-            .then(c => c.PatientProfile)
+        loadComponent: () => import("./patient-profile/patient-profile").then(c => c.PatientProfile)
       }
     ]
   }
